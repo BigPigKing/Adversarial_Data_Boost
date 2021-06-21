[Feat, Fix] Add the accumulated gradient mechinsm. Fix the bug of reinforce reward settings, change the mse reward to jsd reward.

lib/model.py
@@ -167,7 +167,8 @@ class SentimentModel(torch.nn.Module):
     def optimize(
         self,
         loss,
-        optimizers
+        optimizers,
+        is_step: bool = True,
     ):
         loss.backward()
 
@@ -180,8 +181,12 @@ class SentimentModel(torch.nn.Module):
                 )
             else:
                 pass
-            optimizer.step()
-            optimizer.zero_grad()
+
+            if is_step is True:
+                optimizer.step()
+                optimizer.zero_grad()
+            else:
+                pass
 
 
 def main():

lib/reinforcer.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from overrides import overrides
 from typing import List, Dict
+from .loss import JsdCrossEntropy
 from .utils import get_sentence_from_text_field_tensors
 from .augmenter import Augmenter
 from allennlp.data import Vocabulary
@@ -39,7 +40,7 @@ class Environment(torch.nn.Module):
 
         # Calculation Function
         self.cos_similarity = torch.nn.CosineSimilarity()
-        self.mse_loss_reward = torch.nn.MSELoss()
+        self.reinforcer_reward = JsdCrossEntropy()
 
     def get_current_state(
         self
@@ -104,7 +105,7 @@ class Environment(torch.nn.Module):
         if self.cos_similarity(self.encoded_initial_state, encoded_augmented_state) < self.similarity_threshold:
             done = True
             safe = False
-            reward = -0.72
+            reward = -0.024
         else:
             training_status = self.training
             self.eval()
@@ -114,17 +115,11 @@ class Environment(torch.nn.Module):
                 self.train()
             else:
                 pass
-            reward = np.log(
-                self.mse_loss_reward(
-                    self.initial_prediction.detach(),
-                    augmented_prediction.detach()
-                ).cpu().item()
-            )
-            reward = np.clip(reward, -20, -1)
-            reward = 1 / - reward
+
+            reward = self.reinforcer_reward(self.initial_prediction, augmented_prediction).detach().cpu().item()
 
         # Penelty Reward
-        penelty_reward = 0.15 * self.current_step / self.max_step
+        penelty_reward = 0.012 * (self.current_step / self.max_step)
 
         # Record Step
         self.current_step += 1
@@ -146,7 +141,7 @@ class Environment(torch.nn.Module):
         # Last action will be "stop"
         if action == len(self.augmenter_list) - 1:
             done = True
-            reward = 0.24
+            reward = 0.006
         else:
             augmented_state = self.augmenter_list[action].augment(self.current_state)
             reward, done = self._get_env_respond(augmented_state)

lib/trainer.py
@@ -130,12 +130,13 @@ class ReinforceTrainer(Trainer):
 class TextTrainer(Trainer):
     def __init__(
         self,
+        text_trainer_params: Dict,
         train_model: torch.nn.Module,
-        is_save: bool = False
     ):
         super(TextTrainer, self).__init__()
         self.train_model = train_model
-        self.is_save = is_save
+        self.is_save = text_trainer_params["is_save"]
+        self.accumulated_step = text_trainer_params["accumulated_step"]
 
         self.GPU = next(train_model.parameters()).get_device()
 
@@ -196,25 +197,33 @@ class TextTrainer(Trainer):
             # Optimize
             if is_finetune is False:
                 batch_loss = output_dict["classification_loss"]
-                self.train_model.optimize(
-                    batch_loss,
-                    [self.train_model.optimizer]
-                )
                 total_origin_loss += output_dict["classification_loss"]
             else:
                 batch_loss = output_dict["origin_classification_loss"] + 0.18 * output_dict["total_augment_loss"] + output_dict["total_consistency_loss"]  # noqa
-                self.train_model.optimize(
-                    batch_loss,
-                    [self.train_model.optimizer]
-                )
                 total_origin_loss += output_dict["origin_classification_loss"]
                 total_augment_loss += output_dict["total_augment_loss"]
                 total_consistency_loss += output_dict["total_consistency_loss"]
+
+            # Accumulated
+            if (batch_idx+1) % self.accumulated_step == 0:
+                self.train_model.optimize(
+                    batch_loss,
+                    [self.train_model.optimizer],
+                    is_step=True
+                )
+            else:
+                self.train_model.optimize(
+                    batch_loss,
+                    [self.train_model.optimizer],
+                    is_step=False
+                )
 
             num_of_batch += 1
             total_labels.append(batch["label"])
             total_predicts.append(output_dict["predicts"])
             total_loss += batch_loss.item()
+
+        self.train_model.optimizer.zero_grad()
 
         total_labels = torch.cat(total_labels, 0)
         total_predicts = torch.cat(total_predicts, 0)